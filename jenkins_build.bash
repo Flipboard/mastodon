1dc56aba7069e4954947ec5c31ec2b95e979df6b
Greg Scallan <rockhunters08@gmail.com>
2023-02-10 12:32:29 +0000

    upload

--- jenkins_build.bash
+++ jenkins_build.bash
@@ -5,14 +5,20 @@
 set -o errexit
 set -o xtrace
 
-if [[ `whoami` != 'ubuntu' ]]; then
-    echo "This script must be run as ubuntu."
+if [[ $(whoami) != 'ubuntu' ]]; then
+    echo 'This script must be run as ubuntu.'
+    exit 1
+fi
+
+if [[ $# -eq 0 ]] ; then
+    echo 'git commit hash must be passed as an argument'
     exit 1
 fi
 
 SRC_DIR="/mnt/jenkins-mastodon/workspace/mastodon"
 TARGET_DIR="/home/mastodon"
 DATE=$(date +%Y%m%dT%H%M)
+GIT_COMMIT="$1"
 
 # save 1 level of backups
 sudo rm -rfd "${TARGET_DIR}/"backup.*
@@ -31,6 +37,9 @@ sudo chown -R mastodon:mastodon /home/mastodon
 
 # build the code
 sudo -i -u mastodon bash --login -c 'cd live; export PATH=$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH; export RAILS_ENV=production; bundle config deployment "true"; bundle config without "development test"; bundle install; yarn install --pure-lockfile'
+
+# tar and upload
+sudo -i -u mastodon bash --login -c "cp live/updater_post_install.sh updater_post_install.sh; tar cvf ${GIT_COMMIT}.tar live updater_post_install.sh; gzip ${GIT_COMMIT}.tar; aws s3 cp ${GIT_COMMIT}.tar.gz s3://flipboard.prod.releases/mastodon/${GIT_COMMIT}.tar.gz"
 
 exit 0
 
