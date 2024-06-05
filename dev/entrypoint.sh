0dc0fcb331b23781a6d53311df07796a29b3efa2
Greg Melton <gmelton@gmail.com>
2024-06-05 12:17:53 -0700

    this should work with release-42 branch now

--- dev/entrypoint.sh
+++ dev/entrypoint.sh
@@ -4,26 +4,22 @@ set -e # Fail the whole script on first error
 
 # Fetch Ruby gem dependencies
 bundle config path 'vendor/bundle'
-bundle config with 'development test'
+bundle config with 'development'
 bundle install
 
 # Make Gemfile.lock pristine again
 git checkout -- Gemfile.lock
 
 # Fetch Javascript dependencies
-corepack prepare
+# corepack prepare
 yarn install
-
-# # seed test
-# RAILS_ENV=test ./bin/rails db:setup
-# RAILS_ENV=test ./bin/rails db:migrate
-# RAILS_ENV=test ./bin/rails assets:precompile
 
 # You can comment these lines out if you need restart
 # without needing to pickup new changes here...
 # Run db setup/migrations
 RAILS_ENV=development ./bin/rails db:setup
 RAILS_ENV=development ./bin/rails db:migrate
+# Generate public/assets
 RAILS_ENV=development ./bin/rails assets:precompile
 
 # This runs the Procfile.dev apps
