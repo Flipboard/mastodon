203adf70680f32d097fe069d7e87834ecb49fce2
Flipboard Operations <ops@flipboard.com>
2024-08-30 13:53:24 +0000

    add s3 retry for 4.3

--- updater_post_install.sh
+++ updater_post_install.sh
@@ -121,6 +121,8 @@ echo "STATSD_ADDR=localhost:8125" >> /tmp/env.production
 echo "MAX_FOLLOWS_THRESHOLD=750_000" >> /tmp/env.production
 echo "DISABLE_AUTOMATIC_SWITCHING_TO_APPROVED_REGISTRATIONS=true" >> /tmp/env.production
 echo "TRUSTED_PROXY_IP=172.30.0.0/16,127.0.0.1/32" >> /tmp/env.production
+echo "S3_RETRY_LIMIT=2" >> /tmp/env.production
+# MASTODON_USE_LIBVIPS=true
 
 sudo mv "/tmp/env.production" "${TARGET_DIR}/live/.env.production"
 
