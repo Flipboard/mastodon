2a3680df126a3836938abb37bc58026dec942ebe
Greg Scallan <rockhunters08@gmail.com>
2026-06-16 09:35:35 -0400

    4.6 initial merge onto rc2

--- lib/mastodon/prometheus_exporter/local_server.rb
+++ lib/mastodon/prometheus_exporter/local_server.rb
@@ -13,6 +13,13 @@ module Mastodon::PrometheusExporter
 
       # wire up a default local client
       PrometheusExporter::Client.default = PrometheusExporter::LocalClient.new(collector: server.collector)
+    rescue Errno::EADDRINUSE
+      # Another process on this host already owns the exporter server
+      # (e.g. multiple sidekiq services). Fall back to a network client
+      # that sends metrics to the existing server.
+      Rails.logger.info "PrometheusExporter: port #{port} in use, falling back to remote client"
+      PrometheusExporter::Client.default =
+        PrometheusExporter::Client.new(host: bind || 'localhost', port: port)
     end
   end
 end
